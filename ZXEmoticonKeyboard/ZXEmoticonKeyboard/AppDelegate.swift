6a8c4e1287b214ad04dbbfec68513c7a8410822e
windorz <1925449823@qq.com>
2018-04-27 19:35:20 +0800

    剔除 storyBoard 方便移值

--- ZXEmoticonKeyboard/ZXEmoticonKeyboard/AppDelegate.swift
+++ ZXEmoticonKeyboard/ZXEmoticonKeyboard/AppDelegate.swift
@@ -15,7 +15,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow()
+        window?.backgroundColor = UIColor.white
+        
+        let vc = ZXViewController()
+        
+        
+        let nav = UINavigationController.init(rootViewController: vc)
+        
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
